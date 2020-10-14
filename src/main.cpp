5affff7addd4f96f235693e9d6f78132b11da574
milenko-s <starcikm@gmail.com>
2020-10-14 15:57:24 +0200

    Finished output bit vector assembly

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,6 @@ std::deque<bool> count(const unsigned int& a){
 }
 
 // 5.3.1.2 Run length encoding
-// ToDo
 std::deque<bool> run_length_encoding(const std::deque<bool>& a){
     std::deque<bool> output_vector;
     unsigned int zero_counter = 0;
@@ -173,9 +172,14 @@ int main(int argc, char* argv[]){
 
     std::cout << "Welcome to PocketPlus in c++" << std::endl;
 
+    // Constants
+    auto robustness_level_min = std::make_unique<const unsigned int>(0);
+    auto robustness_level_max = std::make_unique<const unsigned int>(7);
+
     // Define inputs and parameters
     auto t = std::make_unique<unsigned int>(0);
     auto input_vector_length = std::make_unique<unsigned int>(8); // F // User defined value
+    auto input_vector_length_count = count(*input_vector_length);
     std::deque<bool> initial_mask_vector;
     initial_mask_vector.assign(*input_vector_length, 0); // M_0 = 0
     auto robustness_level = std::make_unique<unsigned int>(1); // R_t // User defined value
@@ -197,9 +201,11 @@ int main(int argc, char* argv[]){
     mask_build_old.assign(*input_vector_length, 0); // B_t // B_0 = 0
     std::deque<bool> mask_build_new;
     mask_build_new.assign(*input_vector_length, 0); // B_t // B_0 = 0
-    std::deque<bool> mask_change;
-    mask_change.assign(*input_vector_length, 0); // D_t
+    std::deque<std::deque<bool>> mask_change_vector; // D
+    std::deque<bool> mask_change_0; // D_0
+    mask_change_0.assign(*input_vector_length, 0); 
     std::deque<bool> rle_mask_change;
+    std::deque<bool> input_mask_bit_extraction;
     std::deque<bool> first_binary_vector; // h_t Mask change vector
     std::deque<bool> second_binary_vector; // q_t Information about entire mask vector
     std::deque<bool> third_binary_vector ; // u_t either unpredictable bits or the original input I_t
@@ -214,6 +220,7 @@ int main(int argc, char* argv[]){
         throw std::out_of_range("0 <= robustness_level <= 7");
     }
 
+    // ToDo handle the new mask flag correctly ##############################
     new_mask_flag.resize(*robustness_level + 1);
     new_mask_flag.at(0) = false; // User defined value
     if((*robustness_level > 0) && new_mask_flag.at(0) == true){
@@ -235,16 +242,14 @@ int main(int argc, char* argv[]){
     uncompressed_flag = std::make_unique<bool>(1); // r_t
 
     if(!*send_mask_flag && !*uncompressed_flag){
-        throw std::invalid_argument("if send_mask_flag == false -> uncompressed_flag != true");
+        throw std::invalid_argument("if send_mask_flag = = false -> uncompressed_flag != true");
     }
 
     send_input_length_flag = std::make_unique<bool>(0); // v_t // v_0 = 0
 
-    // Section 4.2 Mask update
-    auto first_packet = std::make_unique<bool>(1);
-    
+    // Section 4.2 Mask update    
     // Section 4.2.1
-    if(!*first_packet && !new_mask_flag.at(0)){
+    if(!(*t == 0) && !new_mask_flag.at(0)){
         std::generate(
             mask_build_new.begin(), 
             mask_build_new.end(), 
@@ -257,12 +262,9 @@ int main(int argc, char* argv[]){
     }
     else{
         std::fill(mask_build_new.begin(), mask_build_new.end(), 0);
-        if(*first_packet){
-            first_packet = std::make_unique<bool>(false);
-        }
     }
     // Section 4.2.2
-    if(!first_packet){
+    if(!(*t == 0)){
         if(!new_mask_flag.at(0)){
             std::generate(
                 mask_new.begin(), 
@@ -286,9 +288,13 @@ int main(int argc, char* argv[]){
             );
         }
         // Section 4.2.3
+        while(mask_change_vector.size() > *robustness_level_max + 1){
+            mask_change_vector.pop_front();
+        }
+        mask_change_vector.push_back(mask_change_0);
         std::generate(
-            mask_change.begin(), 
-            mask_change.end(), 
+            mask_change_vector.at(0).begin(), 
+            mask_change_vector.at(0).end(), 
             [mask_new, mask_old, n = 0]() mutable {
                 auto out = mask_new.at(n) ^ mask_old.at(n);
                 n++;
@@ -312,15 +318,27 @@ int main(int argc, char* argv[]){
     }
     else{
         std::deque<bool> X_t;
+        X_t.resize(*input_vector_length);
         if(*robustness_level == 0){
-            X_t = reverse(mask_change);
+            X_t = reverse(mask_change_vector.at(0));
         }
         else if((int)(*t - *robustness_level) <= 0){
-            // D is mask_change
             // X_t = [<D_1 OR D_2 OR ... OR D_t>]
+            X_t.assign(*input_vector_length, 0); 
+            for(unsigned int i = 0; i < mask_change_vector.size(); i++){
+                for(unsigned int index = 0; i < mask_change_vector.at(i).size(); index++){
+                    X_t.at(index) = mask_change_vector.at(i).at(index) || X_t.at(index);
+                }
+            } 
         }
         else{
             // X_t = [<D_(t-robustness_level) OR D_(t-robustness_level)+1 OR ... OR D_t>]
+            X_t.assign(*input_vector_length, 0); 
+            for(unsigned int i = 0; i < *robustness_level + 1; i++){
+                for(unsigned int index = 0; i < mask_change_vector.at(i).size(); index++){
+                    X_t.at(index) = mask_change_vector.at(i).at(index) || X_t.at(index);
+                }
+            }
         }
 
         std::deque<bool> y_t;
@@ -354,27 +372,78 @@ int main(int argc, char* argv[]){
         first_binary_vector.emplace_back(*d_t);
     }
 
-    // 5.3.2.3 second binary vector
-    // ToDo
-    // Second vector: ['1', RLE(mask), '10']
+    // 5.3.2.3 Second binary vector
+    // Second vector: ['1', RLE(< mask ^ mask_<< >), '10']
+    if(*d_t == 1){
+        second_binary_vector = std::deque<bool>();
+    }
+    else if(*send_mask_flag == 1){
+        second_binary_vector = std::deque<bool>();
+        second_binary_vector.insert(second_binary_vector.end(), {1});
+        std::deque<bool> mask_shifted_rle;
+        mask_shifted_rle = mask_new;
+        mask_shifted_rle.pop_front();
+        mask_shifted_rle.push_back({0});
+        std::generate(
+            mask_shifted_rle.begin(), 
+            mask_shifted_rle.end(), 
+            [mask_new, mask_shifted_rle, n = 0]() mutable {
+                auto out = mask_new.at(n) ^ mask_shifted_rle.at(n);
+                n++;
+                return out;
+            }
+        );
+        mask_shifted_rle = reverse(mask_shifted_rle);
+        second_binary_vector.insert(second_binary_vector.end(), mask_shifted_rle.begin(), mask_shifted_rle.end());
+        second_binary_vector.insert(second_binary_vector.end(), {1, 0});
+    }
+    else{
+        second_binary_vector = std::deque<bool>(0);
+    }
 
-    // 5.3.2.4 third binary vector
-    // ToDo
-    //if uncompressed_flag==1 AND send_input_length_flag==0
-    //then ['10' input_new, send_changes_flag]
-    //if uncompressed_flag==1 AND send_input_length_flag==1
-    //then ['11', COUNT(F), input_new, send_changes_flag]
-    //if uncompressed_flag==0
-    //then ['unpredictable bit values defined by the mask']
-    //or, if uncompressed_flag not already indicated by common_settings_flag in h_t (first vector): 
-    //[uncompressed_flag, 'unpredictable bit values defined by the mask']
+    // 5.3.2.4 Third binary vector
+    third_binary_vector = std::deque<bool>();
+    if(*d_t == 1){
+        input_mask_bit_extraction = bit_extraction(input_new , mask_new);
+        third_binary_vector.insert(third_binary_vector.end(), input_mask_bit_extraction.begin(), input_mask_bit_extraction.end());
+    }
+    else if((*uncompressed_flag == 1) && (*send_input_length_flag == 1)){
+        third_binary_vector.insert(third_binary_vector.end(), {1, 1});
+        third_binary_vector.insert(third_binary_vector.end(), input_vector_length_count.begin(), input_vector_length_count.end());
+        third_binary_vector.insert(third_binary_vector.end(), input_new.begin(), input_new.end());
+        third_binary_vector.insert(third_binary_vector.end(), {*send_mask_flag});
+    }
+    else if((*uncompressed_flag == 1) && (*send_input_length_flag == 0)){
+        third_binary_vector.insert(third_binary_vector.end(), {1, 0});
+        third_binary_vector.insert(third_binary_vector.end(), input_new.begin(), input_new.end());
+        third_binary_vector.insert(third_binary_vector.end(), {*send_mask_flag});
+    }
+    else{
+        third_binary_vector.insert(third_binary_vector.end(), {0});
+        input_mask_bit_extraction = bit_extraction(input_new , mask_new);
+        third_binary_vector.insert(third_binary_vector.end(), input_mask_bit_extraction.begin(), input_mask_bit_extraction.end());
+    }
 
     // Output vector: [first, second, third]
     output.insert(output.end(), first_binary_vector.begin(), first_binary_vector.end());
     output.insert(output.end(), second_binary_vector.begin(), second_binary_vector.end());
     output.insert(output.end(), third_binary_vector.begin(), third_binary_vector.end());
 
+    t = std::make_unique<unsigned int>(*t + 1); 
+    input_old = input_new;
+    mask_old = mask_new;
+    mask_build_old = mask_build_new;
+
+    //#######################
+    std::cout << "OUTPUT: " << std::endl;
+    for(auto i: output){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    //#######################
+    
     // Compress more ...
+    // ToDo Construct loop around
 
     // 5.4 Termination
     // 5.4.1.1
