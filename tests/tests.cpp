2b986afb2fc8facbc9dc803e35fc572cec0bb580
milenko-s <starcikm@gmail.com>
2020-10-31 15:11:10 +0100

    Changed GTest in CMake to automatically detect new test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,10 @@
 #include "../src/utils/utils.h"
 #include <gtest/gtest.h>
 
-TEST(DivideUp, InsertZeros){
+TEST(DivideUp, HandlesAllZeroInput){
     ASSERT_THROW(pocketplus::utils::divide_up(0, 0), std::invalid_argument);
-    //ASSERT_EQ(0, pocketplus::utils::divide_up(0, 0));
 }
 
-int main (int argc, char **argv){
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(DivideUp, HandlesZeroInput){
+    ASSERT_EQ(0, pocketplus::utils::divide_up(0, 1));
 }
